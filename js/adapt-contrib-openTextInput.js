a993e405b5dcb9e02336d9c89efef950dcb39161
Thomas Eitler <thomas.eitler@gmail.com>
2014-04-16 14:19:01 +0200

    rebuild component from skeleton

--- js/adapt-contrib-openTextInput.js
+++ js/adapt-contrib-openTextInput.js
@@ -1,114 +1,137 @@
 /*
  * adapt-contrib-openTextInput
  * License - http://github.com/adaptlearning/adapt_framework/LICENSE
- * Maintainers - Kev Adsett <kev.adsett@gmail.com>, Daryl Hedley <darylhedley@hotmail.com>
+ * Maintainers - LearnChamp Petra Nussdorfer <petra.nussdorfer@learnchamp.com>
  */
 
- define(function(require) {
- 	var QuestionView = require('coreViews/questionView');
- 	var Adapt = require('coreJS/adapt');
+define(function(require) {
+    var QuestionView = require('coreViews/questionView');
+    var Adapt = require('coreJS/adapt');
 
- 	var OpenTextInput = QuestionView.extend({
- 		events: {
- 			"click .opentextinput-widget .button.save": "onSaveClicked",
- 			"click .opentextinput-widget .button.submit": "onSubmitClicked",
- 			"click .opentextinput-widget .button.reset": "onResetClicked",
- 			"click .opentextinput-widget .button.model": "onModelAnswerClicked",
- 			"click .opentextinput-widget .button.user": "onUserAnswerClicked",
- 			"blur input": "forceFixedPositionFakeScroll"
- 		},
+    var OpenTextInput = QuestionView.extend({
+        events: {
+            "click .opentextinput-widget .button.save": "onSaveClicked",
+            "click .opentextinput-widget .button.submit": "onSubmitClicked",
+            "click .textinput-widget .button.model":"onModelAnswerClicked",
+            "click .textinput-widget .button.user":"onUserAnswerClicked"
+        },
+        postRender: function() {
+            // IMPORTANT! 
+            // Both of the following methods need to be called inside your view.
 
+            // Use this to set the model status to ready. 
+            // It should be used when telling Adapt that this view is completely loaded.
+            // This is sometimes used in conjunction with imageReady.
+            this.setReadyStatus();
 
- 		setupDefaultSettings: function() {
- 			this.model.set("_isSaved", false);
- 			QuestionView.prototype.setupDefaultSettings.apply(this);
- 		},
+            // Use this to set the model status to complete.
+            // This can be used with inview or when the model is set to complete/the question has been answered.
+            this.setCompletionStatus();
+        },
+        setupDefaultSettings: function() {            
+            this.model.set("_isSaved", false);
 
- 		onSaveClicked: function(event) {
- 			event.preventDefault();
+            QuestionView.prototype.setupDefaultSettings.apply(this);
+        },
 
-  			var userAnswer = this.$(".opentextinput-item-textbox").val();
-  			/*console.log("UserAnswer: " + userAnswer);
-  			console.log("Model Wert",this.model.get("_isSaved"));*/
+        // onSubmitClicked: function(event) {
+        //     event.preventDefault();
+        //     console.log("onSubmitClicked");
+        // },
+        supports_html5_storage: function() {
+            try {
+                return 'localStorage' in window && window['localStorage'] !== null;
+            } catch (e) {
+                return false;
+            }
+        },
+        canSubmit: function() {
+            var canSubmit = true;
+            if ($(".opentextinput-item-textbox").val() == "") {
+                canSubmit = false;
+            }
+            return canSubmit;
+        },
+        forceFixedPositionFakeScroll: function() {
+            if (Modernizr.touch) {
+                _.defer(function() {
+                    window.scrollTo(document.body.scrollLeft, document.body.scrollTop);
+                });
+            }
+        },
+        storeUserAnswer: function() {
+            var userAnswer = this.$(".opentextinput-item-textbox").val();
+            var identifier = this.model.get('_id') + "-OpenTextInput-UserAnswer";
+            // check localstorage
+            if (this.supports_html5_storage()) {
+                localStorage.setItem(identifier, userAnswer);
+                this.model.set("_isSaved", true);
 
-    		// check localstorage
-    		if (this.supports_html5_storage()) {
+            } else {
+                alert("No local storage available");
+            }
+        },
+        onSaveClicked: function(event) {
+            event.preventDefault();
 
-  				localStorage.setItem(this.model.get('_id')+"-OpenTextInput-UserAnswer", userAnswer);
-  				this.model.set("_isSaved", true);
+            /*console.log("UserAnswer: " + userAnswer);
+            console.log("Model Wert",this.model.get("_isSaved"));*/
 
-  			} else {
-  				alert("No local storage available");
-			}
-},
- supports_html5_storage: function() {
-  try {
-    return 'localStorage' in window && window['localStorage'] !== null;
-  } catch (e) {
-    return false;
-  }
-},
+            this.storeUserAnswer();
 
-forceFixedPositionFakeScroll: function() {
-	if (Modernizr.touch) {
-		_.defer(function() {
-			window.scrollTo(document.body.scrollLeft, document.body.scrollTop);
-		});
-	}
-},
+        },
+        onSubmitClicked: function(event) {
+            event.preventDefault();
 
-canSubmit: function() {
-	var canSubmit = true;
-	if ($(".opentextinput-item-textbox").val() == "") {
-		canSubmit = false;
-	}
-	return canSubmit;
-},
+            if (!this.canSubmit()) return;
 
-checkAnswerIsCorrect: function(possibleAnswers, userAnswer) {},
+            Adapt.tabHistory = $(event.currentTarget).parent('.inner');
 
-cleanupUserAnswer: function(userAnswer) {
-	if (this.model.get('_allowsAnyCase')) {
-		userAnswer = userAnswer.toLowerCase();
-	}
-	if (this.model.get('_allowsPunctuation')) {
-		var userAnswerClean = userAnswer.replace(/[\.,-\/#!$£%\^&\*;:{}=\-_`~()]/g, "");
-		userAnswer = $.trim(userAnswerClean);
-	}
-	return userAnswer;
-},
+            this.model.set({
+                _isEnabled: false,
+                _isSubmitted: true,
+            });
+            this.$(".component-widget").addClass("submitted");
 
-markQuestion: function() {},
+            this.storeUserAnswer();
+            // this.showFeedback();
+        },
+        onEnabledChanged: function() {
+            this.$('.opentextinput-item-textbox').prop('disabled', !this.model.get('_isEnabled'));
+        },
+        onModelAnswerShown: function() {
+            this.$(".opentextinput-item-textbox").val(this.mode.get('modelAnswer'));
+        },
+        onUserAnswerShown: function() {
+            var identifier = this.model.get('_id') + "-OpenTextInput-UserAnswer";
+            var userAnswer = '';
+            if (this.supports_html5_storage()) {
+                userAnswer = localStorage.getItem(identifier);
+                this.model.set("_isSaved", true);
 
-onEnabledChanged: function() {
-	this.$('.opentextinput-item-textbox').prop('disabled', !this.model.get('_isEnabled'));
-},
+            } else {
+                alert("No local storage available");
+            }
+            this.$(".opentextinput-item-textbox").val(userAnswer);
 
-onModelAnswerShown: function() {
-	_.each(this.model.get('items'), function(item, index) {
-		this.$(".opentextinput-item-textbox").eq(index).val(item.answers[0]);
-	}, this);
-},
+        },
+        postRender: function() {
+            QuestionView.prototype.postRender.apply(this);
+            this.setReadyStatus();
+        },
+        onComplete: function(parameters) {
+            this.model.set({
+                _isComplete: true,
+                _isEnabled: false,                
+            });
+            this.$(".component-widget").addClass("disabled");
+            // this.showMarking();
+            this.showUserAnswer();
+            Adapt.trigger('questionView:complete', this);
+        },
+        markQuestion: function() {}
+    });
 
-onUserAnswerShown: function() {
-	_.each(this.model.get('items'), function(item, index) {
-		this.$(".opentextinput-item-textbox").eq(index).val(item.userAnswer);
-	}, this);
-},
-
-postRender: function() {
-	QuestionView.prototype.postRender.apply(this);
-	this.setReadyStatus();
-},
-
-storeUserAnswer: function() {
-	_.each(this.model.get('items'), function(item, index) {
-		item.userAnswer = this.$('.opentextinput-item-textbox').eq(index).val();
-	}, this);
-}
-
-});
-
-Adapt.register("opentextinput", OpenTextInput);
+    Adapt.register("opentextinput", OpenTextInput);
 
 });
