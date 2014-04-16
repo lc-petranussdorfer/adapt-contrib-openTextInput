78541f38eeb09c416ac4b2dd3f97d18b343f222c
lc-petranussdorfer <petra.nussdorfer@learnchamp.com>
2014-04-16 12:50:36 +0200

    Save the answer into localstorage

--- js/adapt-contrib-openTextInput.js
+++ js/adapt-contrib-openTextInput.js
@@ -4,107 +4,108 @@
  * Maintainers - Kev Adsett <kev.adsett@gmail.com>, Daryl Hedley <darylhedley@hotmail.com>
  */
 
-define(function(require) {
-	var QuestionView = require('coreViews/questionView');
-	var Adapt = require('coreJS/adapt');
+ define(function(require) {
+ 	var QuestionView = require('coreViews/questionView');
+ 	var Adapt = require('coreJS/adapt');
 
-	var OpenTextInput = QuestionView.extend({
-		events: {
-			"click .opentextinput-widget .button.save": "onSaveClicked",
-			"click .opentextinput-widget .button.submit": "onSubmitClicked",
-			"click .opentextinput-widget .button.reset": "onResetClicked",
-			"click .opentextinput-widget .button.model": "onModelAnswerClicked",
-			"click .opentextinput-widget .button.user": "onUserAnswerClicked",
-			"blur input": "forceFixedPositionFakeScroll"
-		},
+ 	var OpenTextInput = QuestionView.extend({
+ 		events: {
+ 			"click .opentextinput-widget .button.save": "onSaveClicked",
+ 			"click .opentextinput-widget .button.submit": "onSubmitClicked",
+ 			"click .opentextinput-widget .button.reset": "onResetClicked",
+ 			"click .opentextinput-widget .button.model": "onModelAnswerClicked",
+ 			"click .opentextinput-widget .button.user": "onUserAnswerClicked",
+ 			"blur input": "forceFixedPositionFakeScroll"
+ 		},
 
 
- setupDefaultSettings: function() {
- 	this.model.set("_isSaved", false);
+ 		setupDefaultSettings: function() {
+ 			this.model.set("_isSaved", false);
 
- 	QuestionView.prototype.setupDefaultSettings.apply(this);
- },
+ 			QuestionView.prototype.setupDefaultSettings.apply(this);
+ 		},
 
-  onSaveClicked: function(event) {
-  	 event.preventDefault();
+ 		onSaveClicked: function(event) {
+ 			event.preventDefault();
   	// ToDo
 
 
   	// Console log
-   var userAnswer = this.$(".opentextinput-item-textbox").val();
-   console.log("UserAnswer: " + userAnswer);
-   console.log("Model Wert",this.model.get("_isSaved"));
+  	var userAnswer = this.$(".opentextinput-item-textbox").val();
+  	console.log("UserAnswer: " + userAnswer);
+  	console.log("Model Wert",this.model.get("_isSaved"));
 
     // check localstorage
+    if (Modernizr.localstorage) {
+    	
+  		localStorage.setItem("Answer", userAnswer);
+  		this.model.set("_isSaved", true);
 
-    	// if yes
-    	// Write to localstorage
-    	 this.model.set("_isSaved", true);
+  	} else {
+  		alert("No local storage available");
+	}
+},
 
-    	// if no
-    	//alert("No local storage available");
-  },
+forceFixedPositionFakeScroll: function() {
+	if (Modernizr.touch) {
+		_.defer(function() {
+			window.scrollTo(document.body.scrollLeft, document.body.scrollTop);
+		});
+	}
+},
 
-		forceFixedPositionFakeScroll: function() {
-			if (Modernizr.touch) {
-				_.defer(function() {
-					window.scrollTo(document.body.scrollLeft, document.body.scrollTop);
-				});
-			}
-		},
+canSubmit: function() {
+	var canSubmit = true;
+	if ($(".opentextinput-item-textbox").val() == "") {
+		canSubmit = false;
+	}
+	return canSubmit;
+},
 
-		canSubmit: function() {
-			var canSubmit = true;
-			if ($(".opentextinput-item-textbox").val() == "") {
-				canSubmit = false;
-			}
-			return canSubmit;
-		},
+checkAnswerIsCorrect: function(possibleAnswers, userAnswer) {},
 
-		checkAnswerIsCorrect: function(possibleAnswers, userAnswer) {},
+cleanupUserAnswer: function(userAnswer) {
+	if (this.model.get('_allowsAnyCase')) {
+		userAnswer = userAnswer.toLowerCase();
+	}
+	if (this.model.get('_allowsPunctuation')) {
+		var userAnswerClean = userAnswer.replace(/[\.,-\/#!$£%\^&\*;:{}=\-_`~()]/g, "");
+		userAnswer = $.trim(userAnswerClean);
+	}
+	return userAnswer;
+},
 
-		cleanupUserAnswer: function(userAnswer) {
-			if (this.model.get('_allowsAnyCase')) {
-				userAnswer = userAnswer.toLowerCase();
-			}
-			if (this.model.get('_allowsPunctuation')) {
-				var userAnswerClean = userAnswer.replace(/[\.,-\/#!$£%\^&\*;:{}=\-_`~()]/g, "");
-				userAnswer = $.trim(userAnswerClean);
-			}
-			return userAnswer;
-		},
+markQuestion: function() {},
 
-		markQuestion: function() {},
+onEnabledChanged: function() {
+	this.$('.opentextinput-item-textbox').prop('disabled', !this.model.get('_isEnabled'));
+},
 
-		onEnabledChanged: function() {
-			this.$('.opentextinput-item-textbox').prop('disabled', !this.model.get('_isEnabled'));
-		},
+onModelAnswerShown: function() {
+	_.each(this.model.get('items'), function(item, index) {
+		this.$(".opentextinput-item-textbox").eq(index).val(item.answers[0]);
+	}, this);
+},
 
-		onModelAnswerShown: function() {
-			_.each(this.model.get('items'), function(item, index) {
-				this.$(".opentextinput-item-textbox").eq(index).val(item.answers[0]);
-			}, this);
-		},
+onUserAnswerShown: function() {
+	_.each(this.model.get('items'), function(item, index) {
+		this.$(".opentextinput-item-textbox").eq(index).val(item.userAnswer);
+	}, this);
+},
 
-		onUserAnswerShown: function() {
-			_.each(this.model.get('items'), function(item, index) {
-				this.$(".opentextinput-item-textbox").eq(index).val(item.userAnswer);
-			}, this);
-		},
+postRender: function() {
+	QuestionView.prototype.postRender.apply(this);
+	this.setReadyStatus();
+},
 
-		postRender: function() {
-			QuestionView.prototype.postRender.apply(this);
-			this.setReadyStatus();
-		},
+storeUserAnswer: function() {
+	_.each(this.model.get('items'), function(item, index) {
+		item.userAnswer = this.$('.opentextinput-item-textbox').eq(index).val();
+	}, this);
+}
 
-		storeUserAnswer: function() {
-			_.each(this.model.get('items'), function(item, index) {
-				item.userAnswer = this.$('.opentextinput-item-textbox').eq(index).val();
-			}, this);
-		}
+});
 
-	});
-
-	Adapt.register("opentextinput", OpenTextInput);
+Adapt.register("opentextinput", OpenTextInput);
 
 });
